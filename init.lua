9715de5ea087933151f7fa1c6f90b1196f2d7a6e
Yiu Man Ho <yiufamily.hh@gmail.com>
2020-08-11 16:06:28 +0800

    Add lava bucket support

--- init.lua
+++ init.lua
@@ -9,6 +9,20 @@ local modname = minetest.get_current_modname()
 
 local hasareasmod = minetest.get_modpath("areas")
 
+
+local function check_protection(pos, name, text)
+	if minetest.is_protected(pos, name) then
+		minetest.log("action", (name ~= "" and name or "A mod")
+			.. " tried to " .. text
+			.. " at protected position "
+			.. minetest.pos_to_string(pos)
+			.. " with a bucket")
+		minetest.record_protection_violation(pos, name)
+		return true
+	end
+	return false
+end
+
 -- The settings object
 local settings = {}
 if minetest.settings then
@@ -136,7 +150,7 @@ minetest.register_chatcommand("anti_lava_area", {
 
 -- Register place lava callback.
 minetest.register_on_placenode(function(pos, newnode, placer, oldnode, itemstack, pointed_thing)
-	if newnode.name == "default:lava_source" then
+	if newnode.name == "default:lava_source" or (newnode.name == "default:lava_source" and (placer:get_wielded_item().name == "bucket:bucket_lava" or placer:get_wielded_item().name == "bucket:bucket_empty")) then
 		local name = placer:get_player_name()
 		if name then
 			local can_ignore, missing_privs = minetest.check_player_privs(name, {anti_lava_area_ignore=true})
@@ -153,6 +167,70 @@ minetest.register_on_placenode(function(pos, newnode, placer, oldnode, itemstack
 		end
 	end
 end)
+-- bucket:bucket_lava
+minetest.override_item("bucket:bucket_lava", {
+    	on_place = function(itemstack, user, pointed_thing)
+		-- Must be pointing to node
+		if pointed_thing.type ~= "node" then
+			return
+		end
+	
+		local node = minetest.get_node_or_nil(pointed_thing.under)
+		local ndef = node and minetest.registered_nodes[node.name]
+	
+		-- Call on_rightclick if the pointed node defines it
+		if ndef and ndef.on_rightclick and
+				not (user and user:is_player() and
+				user:get_player_control().sneak) then
+			return ndef.on_rightclick(
+				pointed_thing.under,
+				node, user,
+				itemstack)
+		end
+	
+		local lpos
+	
+		-- Check if pointing to a buildable node
+		if ndef and ndef.buildable_to then
+			-- buildable; replace the node
+			lpos = pointed_thing.under
+		else
+			-- not buildable to; place the liquid above
+			-- check if the node above can be replaced
+	
+			lpos = pointed_thing.above
+			node = minetest.get_node_or_nil(lpos)
+			local above_ndef = node and minetest.registered_nodes[node.name]
+	
+			if not above_ndef or not above_ndef.buildable_to then
+				-- do not remove the bucket with the liquid
+				return itemstack
+			end
+		end
+	
+		if check_protection(lpos, user
+				and user:get_player_name()
+				or "", "place ".."default:lava_source") then
+			return
+		end
+		
+		local name = user:get_player_name()
+		if name then
+			local can_ignore, missing_privs = minetest.check_player_privs(name, {anti_lava_area_ignore=true})
+			if not(can_ignore) then
+				for k, v in pairs(anti_lava_area_store:get_areas_for_pos(lpos)) do
+					if k then
+						minetest.chat_send_player(name, "You can't place lava here!")
+						return
+					end
+				end
+			end
+			minetest.chat_send_player(name, "[DEBUG] Place lava sucess!")
+		end
+		minetest.set_node(lpos, {name = "default:lava_source"})
+		return ItemStack("bucket:bucket_empty")
+	end
+})
 
 
 if hasareasmod then
